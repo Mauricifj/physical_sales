c2cdd02d546e1c727332f25bf012e5e99eaf4a1f
Maurici Ferreira Junior <m.f.j@hotmail.com>
2020-05-03 11:49:02 -0300

    Merged credentials.go

--- oauth.go
+++ oauth.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"github.com/go-resty/resty/v2"
+	"gopkg.in/yaml.v3"
+	"os"
 )
 
 func GetAccessToken() *OAuthResponse {
@@ -44,8 +46,32 @@ func setupOAuthRequest() *resty.Request {
 	return request
 }
 
+func GetCredentials() *Credentials {
+	credentials := &Credentials{}
+	path := "./credentials.yml"
+
+	file, err := os.Open(path)
+	if err != nil {
+		panic(fmt.Errorf("Fatal error while opening credentials: %s \n", err))
+	}
+	defer file.Close()
+
+	decoder := yaml.NewDecoder(file)
+
+	if err := decoder.Decode(&credentials); err != nil {
+		panic(fmt.Errorf("Fatal error while decoding credentials: %s \n", err))
+	}
+
+	return credentials
+}
+
 type OAuthResponse struct {
 	AccessToken string `json:"access_token"`
 	TokenType   string `json:"token_type"`
 	ExpiresIn   int    `json:"expires_in"`
+}
+
+type Credentials struct {
+	Username string `yaml:username`
+	Password string `yaml:password`
 }
